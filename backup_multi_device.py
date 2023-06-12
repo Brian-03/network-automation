4897f41362159ba74b82af39d0e638e8cefb8f82
Brian-03 <63219369+Brian-03@users.noreply.github.com>
2023-06-12 13:36:04 +0700

    Update and rename lab_netmiko_file.py to backup_multi_device.py

--- backup_multi_device.py
+++ backup_multi_device.py
@@ -4,10 +4,17 @@ data_router=open('data_router.csv','r').readlines()
 data_router.remove(data_router[0])
 
 
-list_config=['int lo99',
-            'description create by netmiko',
+list_config=[#'int lo99',
+            #'description create by netmiko',
             # 'ip add 9.9.9.9 255.255.255.255',
-            'do show ip int brief']
+            'vlan 192',
+            'name tes',
+            'vlan 193',
+            'name tes1',
+            'vlan 195',
+            'name Tesss',
+            'do wr',
+            'do show vlan br']
 
 for router in data_router:
     router=router.split(',')
@@ -29,11 +36,12 @@ for router in data_router:
     net_connect=ConnectHandler(**router_dict)
     net_connect.enable()
     output=net_connect.send_config_set(list_config)
-    print(output)
+    get_hostname = net_connect.send_command("show running-config | s hostname")
 
+    print(output)
     #export configuration
     verif=net_connect.send_multiline(commands)
-    save=open(f'ccna_{router[0]}.txt','w')
+    save=open(f'GAMA_{get_hostname.split(" ")[1]}.txt','w')
     save.write(verif)
     save.close()
 
